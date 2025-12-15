1645f19038cd0797a85beed69e7df43649183f6f
Sascha Friedli <sascha.friedli@students.unibe.ch>
2025-12-15 21:49:07 +0100

    Refactoring report: cleaned up graphics

---
+++ R/lookup_table.R
@@ -0,0 +1,20 @@
+# Look-Up Table for station name and abbreviation
+station_lookup <- tibble::tribble(
+  ~site, ~station_full,
+  "AIG", "Aigle",
+  "EBK", "Ebnat_Kappel",
+  "GVE", "Genf",
+  "GUT", "Guettingen",
+  "HLL", "Hallau",
+  "KOP", "Koppigen",
+  "LUZ", "Luzern",
+  "PAY", "Payerne",
+  "SHA", "Schaffhausen",
+  "SIO", "Sion",
+  "STG", "St.Gallen",
+  "TAE", "Aadorf",      
+  "WAE", "Waedenswil",
+  "REH", "Affoltern",
+  "CGI", "Nyon"
+  
+)
